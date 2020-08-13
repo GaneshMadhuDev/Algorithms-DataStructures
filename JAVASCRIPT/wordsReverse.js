b2ce2e7b12554f1bed3053b76cf07452e205231d
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-13 09:53:46 -0500

    WORDS REVERSE PROBLEM FOR A GIVEN STRING

---
+++ JAVASCRIPT/wordsReverse.js
@@ -0,0 +1,23 @@
+/**
+  * PROGRAM TO REVERSE THE GIVEN STRING ??
+  *
+  */
+
+
+  function wordReverse(givenString) {
+
+
+        return givenString.split(' ').reverse();
+
+  }
+
+
+
+  /** TESTING
+      * GIVEN STRING = "GANESH MADHU"
+      *
+      * wordReverse("GANESH MADHU")
+      *
+      * EXPECTED RESULT    ["MADHU" , "GANESH"]
+      *
+      */
