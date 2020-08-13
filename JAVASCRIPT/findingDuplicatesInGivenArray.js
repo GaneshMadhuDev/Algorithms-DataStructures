89aed7aeb34f53e7fda109a61787310e9d30edaa
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-13 09:40:19 -0500

    FINDING DUPLICATES EXISTS IN AN ARRAY

--- JAVASCRIPT/findingDuplicatesInGivenArray.js
+++ JAVASCRIPT/findingDuplicatesInGivenArray.js
@@ -1,1 +1,46 @@
-A
+/**
+  * PROGRAM TO FIND GIVEN ARRAY HAS THE DUPLICATE ELEMENTS EXISTS ??
+  *
+  */
+
+
+  function isDuplicatesExists(givenArray) {
+
+     let elements = {};
+
+     for (let index = 0; index < givenArray.length; index++) {
+
+        if (!elements[givenArray[index]]) {
+
+            elements[givenArray[index]] = true;
+
+        } else {
+          return true;
+        }
+
+     }
+
+  }
+
+
+
+  /** TESTING
+    * GIVEN ARRAY = [3, 5, 3, 6, 2, 5, 7]
+    *
+    * isDuplicatesExists([3, 5, 3, 6, 2, 5, 7])
+    *
+    * EXPECTED RESULT    TRUE
+    *
+    */
+
+
+
+
+  /** TESTING
+      * GIVEN ARRAY = [3, 4, 8, 6, 2, 5, 7]
+      *
+      * isDuplicatesExists([3, 4, 8, 6, 2, 5, 7])
+      *
+      * EXPECTED RESULT    FALSE
+      *
+      */
