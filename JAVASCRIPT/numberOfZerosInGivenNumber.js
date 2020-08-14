50fb0968e236121f2a083447f5f316fdca0d40ce
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-14 09:46:02 -0500

    Adding Test results to find the number of zeros Upto The given Number

--- JAVASCRIPT/numberOfZerosInGivenNumber.js
+++ JAVASCRIPT/numberOfZerosInGivenNumber.js
@@ -22,3 +22,22 @@
 
 
 
+  /** TESTING
+      * GIVEN NUMBER = 30;
+      *
+      * findNumberOfZerosInGivenNumber(30)
+      *
+      * EXPECTED RESULT     3 => 10, 20, 30
+      *
+      */
+
+
+  /** TESTING
+        * GIVEN NUMBER = 101;
+        *
+        * findNumberOfZerosInGivenNumber(101)
+        *
+        * EXPECTED RESULT     11 => 10, 20, 30, 40, 50, 60, 70, 80, 90, 100
+        *                            |   |   |   |   |   |   |   |   |   ||
+        *
+        */
