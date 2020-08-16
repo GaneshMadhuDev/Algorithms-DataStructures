2437ff1a23a9d1c8c3ca18ae557d8ec269150d32
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-15 22:58:49 -0500

    Adding Code for sum finding in an array

--- JAVASCRIPT/findSumOfTwoNumbersExistInGivenArray.js
+++ JAVASCRIPT/findSumOfTwoNumbersExistInGivenArray.js
@@ -6,7 +6,26 @@
 
   function isSumExistsInGivenArray(givenArray, sumOfTwoNumbers) {
 
+        let object = {};
+        let  arrayLength = givenArray.length;
+        let subtractedNumber;
 
+        for (let index = 0; index < arrayLength; index ++) {
 
+            subtractedNumber = sumOfTwoNumbers - givenArray[index];
+
+            if (object[subtractedNumber]) {
+
+                return true;
+
+            } else {
+
+                object[givenArray[index]] = true;
+
+            }
+
+        }
+
+        return false;
 
   }
