2cf4d0dd3be1da25c0a1da563230eed0d80c2308
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-16 09:27:32 -0500

    Program for printAllDivisorsOfAGivenNumberBetweenRange

--- JAVASCRIPT/printAllDivisorsOfAGivenNumberBetweenRange.js
+++ JAVASCRIPT/printAllDivisorsOfAGivenNumberBetweenRange.js
@@ -4,8 +4,28 @@
  */
 
 
- function printAllDivisorsOfAGivenNUmberBetweenRange() {
+ function printAllDivisorsOfAGivenNUmberBetweenRange(lowNUmber, highNUmber, givenDivisor) {
 
 
+    let printObject = {};
+
+    for (let index = lowNUmber; index <= highNUmber; index ++) {
+
+        printObject[index] = index % givenDivisor === 0;
+
+    }
+
+    return printObject;
 
  }
+
+
+ /** TESTING
+      *  lowNUmber = 4
+      *  highNUmber = 17
+      *  givenDivisor = 3;
+      *  printAllDivisorsOfAGivenNUmberBetweenRange(4, 17, 3)
+      *
+      * EXPECTED RESULT   { 5: false, 6 : true, 7 : false, 8: false, 9: true, 10: false, 11: false, 12: true, 13: false, 14:false, 15: true, 16: false }
+      *
+      */
