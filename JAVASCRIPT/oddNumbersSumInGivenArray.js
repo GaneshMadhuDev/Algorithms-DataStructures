c3e5d02bd06ea8414c50b81264ababdd7030423a
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-08-17 11:29:25 -0500

    code for sum of odd numbers in an array

--- JAVASCRIPT/oddNumbersSumInGivenArray.js
+++ JAVASCRIPT/oddNumbersSumInGivenArray.js
@@ -2,3 +2,20 @@
  * PROGRAM TO FIND THE SUM OF ALL ODD NUMBERS IN AN GIVEN ARRAY.
  *
  */
+
+
+ function sumOfAllOddNumbersInArray(givenArray) {
+
+    let sum = 0;
+
+    for (let index = 0; index < givenArray.length; index ++) {
+
+        if (givenArray[index] % 2 === 1) {
+            sum += givenArray[index];
+        }
+
+    }
+
+    return sum;
+
+ }
