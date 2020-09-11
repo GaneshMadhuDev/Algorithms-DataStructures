1a4742445fe2abb5707c311f8766d4e65e32d0f0
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-09-11 09:49:17 -0500

    Square root of a given number

---
+++ JAVASCRIPT/squareRootOfGivenNumber.js
@@ -0,0 +1,32 @@
+
+/** Given a Number find the square root*/
+
+
+
+function mySqrt(x) {
+
+  if (x === 1) return 1;
+  let min = 0;
+  let max = Number(x / 2);
+  let mid = Number((max + min) / 2);
+  let ans;
+  while (!ans) {
+    if (mid ** 2 === x) {
+      return mid;
+    }
+    if (mid ** 2 < x) {
+      min = mid + 1;
+    } else {
+      max = mid - 1;
+    }
+    if (min === max) return min;
+    if (min + 1 === max && min ** 2 < x && max ** 2 > x) return min;
+    if (mid === Number((max + min) / 2)) return mid;
+    mid = Number((max + min) / 2);
+  }
+  return ans;
+
+}
+
+
+// mySqrt ( 144) => 12
