2e3e15f7bcd05c7e8b3185838c1015c9c26e9aed
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-09-22 13:26:47 -0500

    INVERT A BINARY TREE

---
+++ JAVASCRIPT/invertBinaryTree.js
@@ -0,0 +1,21 @@
+
+
+/*** INVERT A BINARY TREE****/
+
+function invertTree(head) {
+
+  if (head) {
+   var temp = head.left;
+    head.left = head.right;
+    head.right = temp;
+
+    invertTree(head.left);
+    invertTree(head.right);
+
+  }
+  return head;
+
+
+}
+
+
