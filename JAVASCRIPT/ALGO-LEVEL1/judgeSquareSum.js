ba0e6bf5b803550609e1eaa426cffff71579248d
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-10-01 16:35:18 -0500

    Sum of Square Numbers

---
+++ JAVASCRIPT/ALGO-LEVEL1/judgeSquareSum.js
@@ -0,0 +1,35 @@
+/*
+Sum of Square Numbers
+
+Given a non-negative integer c, your task is to decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: 5
+Output: True
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: 3
+Output: False
+*/
+
+/**
+ * @param {number} c
+ * @return {boolean}
+ */
+var judgeSquareSum = function(c) {
+  var iter = 0;
+  var set = new Set();
+  while(iter ** 2 <= c) {
+    var square = iter * iter;
+    if(square * 2 === c || set.has(c - square))
+      return true;
+
+    set.add(square);
+    iter++;
+  }
+
+  return false;
+};
+
+module.exports.judgeSquareSum = judgeSquareSum;
