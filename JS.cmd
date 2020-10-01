2b17f647617625bd9e576832dc0f1b44013df314
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-10-01 16:20:33 -0500

    Level 1 Programs Binary Gap, Majority Elements

---
+++ JS.cmd
@@ -0,0 +1,114 @@
+
+
+HOISTING
+========
+
+VARIABLE HOSISTING IS RARE
+
+FUNCTION HOISTING IS COMMON.
+
+HOSITING IS THE JS INTERPRETERS ACTION OF MOVING ALL VARIABLE AND FUNCTION DECLARATIONS TO THE TOP OF THE CURRENT SCOPE.
+
+
+
+Generator In JS
+===============
+
+
+
+
+OBJECT.FREEZE (VS) CONST
+========================
+
+
+
+
+ENUMS IN JS
+===========
+
+
+
+
+
+
+ES6 CLASSES
+===========
+
+
+
+
+
+
+CALL VS APPLY
+=============
+
+
+
+
+
+SPREAD SYNTAX IN ES6
+====================
+
+
+
+
+
+ARROW FUNCTIONS IN ES6
+======================
+
+
+
+
+
+SPREAD OPERATOR
+===============
+
+
+
+
+
+PROMISES
+========
+
+
+
+
+
+
+IIFE => IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+
+
+
+
+
+
+
+
+
+DEFAULT PARAMETER VALUES
+
+
+
+
+
+
+
+FOR OF OPERATOR
+===============
+
+
+
+
+
+
+
+CONST
+=====
+
+
+
+
+
+
+
+
