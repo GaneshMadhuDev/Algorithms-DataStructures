5da9fbe6f028a6976945f198687c8b0d6555a7e6
Ganesh madhu <dev.ganeshmadhu@gmail.com>
2020-10-02 22:44:02 -0500

    Count the number of segments in a string

---
+++ JAVASCRIPT/ALGO-LEVEL1/countSegmentsInString.js
@@ -0,0 +1,36 @@
+/*
+Number of Segments in a String
+
+
+Count the number of segments in a string, where a segment is defined to be a contiguous sequence of non-space characters.
+
+Please note that the string does not contain any non-printable characters.
+
+Example:
+
+Input: "Hello, my name is John"
+Output: 5
+*/
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSegments = function(s) {
+  var count = 0;
+  var i = 0;
+
+  while(i < s.length) {
+    if(s[i] !== " ") {
+      count++;
+      while(i < s.length && s[i] !== " ")
+        i++;
+    }
+    i++;
+  }
+
+  return count;
+};
+
+module.exports.countSegments = countSegments;
